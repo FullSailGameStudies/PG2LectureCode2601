7db2c468635473ca015ce48c4a085bf225b4fb7d
Garrett Girod <girodg@hotmail.com>
2026-01-12 11:53:40 -0500

    bubblesort

--- Week 2/Day04/Day04/Day04.cpp
+++ Week 2/Day04/Day04/Day04.cpp
@@ -11,10 +11,10 @@
 
 std::string toUpper(const std::string& original)
 {
-    std::string copy = original;
-    for (auto& c : copy)
-        c = toupper(c);
-    return copy;
+	std::string copy = original;
+	for (auto& c : copy)
+		c = toupper(c);
+	return copy;
 }
 
 //ALL recursive loops REQUIRE...
@@ -23,162 +23,200 @@ std::string toUpper(const std::string& original)
 // 2) a recursive condition (a line that calls the method)
 void recursiveLoop(int N)
 {
-    if (N < 10)
-    {
-        std::cout << N << " ";
-        recursiveLoop(N + 1);
-        Console::Write(N, ConsoleColor::Red);
-    }
+	if (N < 10)
+	{
+		std::cout << N << " ";
+		recursiveLoop(N + 1);
+		Console::Write(N, ConsoleColor::Red);
+	}
 }
 
 unsigned long factorial(unsigned int N)
 {
-    if (N <= 1) return 1; //here's the exit condition!
-    return N * factorial(N - 1);
+	if (N <= 1) return 1; //here's the exit condition!
+	return N * factorial(N - 1);
 }
 
 /*
-    ╔═══════╗
-    ║Sorting║
-    ╚═══════╝
+	╔═══════╗
+	║Sorting║
+	╚═══════╝
 
-    Sorting is used to order the items in a vector/array is a specific way
+	Sorting is used to order the items in a vector/array is a specific way
 
-    CHALLENGE 2:
+	CHALLENGE 2:
 
-        Convert this BubbleSort pseudo-code into a method
+		Convert this BubbleSort pseudo-code into a method
 
-        procedure bubbleSort(A : list of sortable items)
-          n := length(A)
-          repeat
-              swapped := false
-              for i := 1 to n - 1 inclusive do
-                  if A[i - 1] > A[i] then
-                      swap(A, i - 1, i)
-                      swapped = true
-                  end if
-              end for
-              n := n - 1
-          while swapped
-        end procedure
+procedure bubbleSort(A : list of sortable items)
+	n := length(A)
+	repeat
+		swapped := false
+		for i := 1 to n - 1 inclusive do
+			if A[i - 1] > A[i] then
+				swap(A, i - 1, i)
+				swapped = true
+			end if
+		end for
+		n := n - 1
+	while swapped
+end procedure
 
 */
 
+//procedure bubbleSort(A : list of sortable items)
+void bubbleSort(std::vector<std::string>& A)
+{
+	//n := length(A)
+	int n = A.size();
+	//repeat
+
+	bool swapped = false;
+	do
+	{
+		//swapped: = false
+		swapped = false;
+
+		//for i := 1 to n - 1 inclusive do
+		for (int i = 1; i <= n - 1; i++)//do
+		{
+			//if A[i - 1] > A[i] then
+			if (A[i - 1] > A[i])
+			{
+				//TODO: figure how to swap
+				//swap(A, i - 1, i) ???
+				std::swap(A[i - 1], A[i]);
+				//OG
+				//std::string temp = A[i - 1];
+				//A[i - 1] = A[i];
+				//A[i] = temp;
+
+				//swapped = true
+				swapped = true;
+			}//end if
+		}//end for
+		//n := n - 1
+		--n;
+	} while (swapped);
+	//while swapped
+}//end procedure
+
 void Bats(int i)
 {
-    if(i < 100)//exit condition (base case)
-    {
-        std::cout << (char)78 << (char)65 << ' ';
-        //++i pre-increment
-        //i++ post-increment
-        Bats(++i);//recursive condition
-    }
+	if (i < 100)//exit condition (base case)
+	{
+		std::cout << (char)78 << (char)65 << ' ';
+		//++i pre-increment
+		//i++ post-increment
+		Bats(++i);//recursive condition
+	}
 }
 
 int main()
 {
-    std::vector<std::string> names = { "Wonder Woman", "Superman", "Batman", "Flash", "Aquaman" };
-    //call your BubbleSort on the names vector.
+	std::vector<std::string> names = { "Wonder Woman", "Superman", "Batman", "Flash", "Aquaman" };
+	//call your BubbleSort on the names vector.
 
-    Console::WriteLine("--UNSORTED--", ConsoleColor::Yellow);
-    //print the sorted vector.
-    for (auto& name : names)
-        std::cout << name << "\n";
+	Console::WriteLine("--UNSORTED--", ConsoleColor::Yellow);
+	//print the sorted vector.
+	for (auto& name : names)
+		std::cout << name << "\n";
 
-    //call BubbleSort
+	//call BubbleSort
+	bubbleSort(names);
 
-    Console::WriteLine("--SORTED--", ConsoleColor::Yellow);
-    //print the sorted vector.
-    for (auto& name : names)
-        std::cout << name << "\n";
-
-
-    /*
-        ╔═════════╗
-        ║Recursion║
-        ╚═════════╝
-
-        Recursion happens when a method calls itself. This creates a recursive loop.
-
-        All recursive methods need an exit condition, something that prevents the loop from continuing.
-
-    */
-    for (int i = 0; i < 10; i++)
-    {
-
-    }
-    while (true)
-    {
-        break;
-    }
-    int N = 0;
-    recursiveLoop(N);
+	Console::WriteLine("--SORTED--", ConsoleColor::Yellow);
+	//print the sorted vector.
+	for (auto& name : names)
+		std::cout << name << "\n";
 
 
-    /*
-        CHALLENGE 1:
+	/*
+		╔═════════╗
+		║Recursion║
+		╚═════════╝
 
-            convert this for loop to a recursive method called Bats.
-            Call Bats here in Main.
+		Recursion happens when a method calls itself. This creates a recursive loop.
 
-            for(int i = 0;i < 100;i++)
-            {
-                std::cout << (char)78 << (char)65 << ' ';
-            }
-    */
-    //call Bats here.
-    int i = 0;
-    Bats(i);
-    char c[] = { '\n', 66, 65, 84, 77, 65, 78, 33, 33 };
-    for (auto ch : c) std::cout << ch;
+		All recursive methods need an exit condition, something that prevents the loop from continuing.
+
+	*/
+	for (int i = 0; i < 10; i++)
+	{
+
+	}
+	while (true)
+	{
+		break;
+	}
+	int N = 0;
+	recursiveLoop(N);
+
+
+	/*
+		CHALLENGE 1:
+
+			convert this for loop to a recursive method called Bats.
+			Call Bats here in Main.
+
+			for(int i = 0;i < 100;i++)
+			{
+				std::cout << (char)78 << (char)65 << ' ';
+			}
+	*/
+	//call Bats here.
+	int i = 0;
+	Bats(i);
+	char c[] = { '\n', 66, 65, 84, 77, 65, 78, 33, 33 };
+	for (auto ch : c) std::cout << ch;
 
 
 
-    /*
-        ╔═════════════════╗
-        ║  Swapping Items ║
-        ╚═════════════════╝
+	/*
+		╔═════════════════╗
+		║  Swapping Items ║
+		╚═════════════════╝
 
-        add #include <string_view>
-        make sure the project is using C++17 or greater.
+		add #include <string_view>
+		make sure the project is using C++17 or greater.
 
-    */
-    std::cout << "\n\n---SWAPPING ITEMS---\n";
-    std::vector<int> nums = { 1,2,3,4,5 };
-    for (auto i : nums)
-        std::cout << i << " ";
-    std::cout << " (original)\n";
+	*/
+	std::cout << "\n\n---SWAPPING ITEMS---\n";
+	std::vector<int> nums = { 1,2,3,4,5 };
+	for (auto i : nums)
+		std::cout << i << " ";
+	std::cout << " (original)\n";
 
-    //write code to swap 2 items in the vector
+	//write code to swap 2 items in the vector
 
-    std::cout << " (after swapping)\n";
-    for (auto i : nums)
-        std::cout << i << " ";
+	std::cout << " (after swapping)\n";
+	for (auto i : nums)
+		std::cout << i << " ";
 
-    /*
-        ╔═════════════════╗
-        ║Comparing Strings║
-        ╚═════════════════╝
+	/*
+		╔═════════════════╗
+		║Comparing Strings║
+		╚═════════════════╝
 
-        the compare functions return an int that will tell you 
-        how the strings compare to each other.
-        < 0 means s1 is LESS THAN (or comes before) s2
-          0 means s1 is EQUAL TO s2
-        > 0 means s1 is GREATER THAN (or comes after) s2
+		the compare functions return an int that will tell you
+		how the strings compare to each other.
+		< 0 means s1 is LESS THAN (or comes before) s2
+		  0 means s1 is EQUAL TO s2
+		> 0 means s1 is GREATER THAN (or comes after) s2
 
-        _stricmp: case-insensitive string comparison. 
-            The methods needs pointers to char arrays therefore 
-            you need to call c_str on the std::string
+		_stricmp: case-insensitive string comparison.
+			The methods needs pointers to char arrays therefore
+			you need to call c_str on the std::string
 
-        std::string::compare:  case-sensitive string comparison
-            If you want to ignore case, you can convert the strings 
-            to all uppercase/lowercase
+		std::string::compare:  case-sensitive string comparison
+			If you want to ignore case, you can convert the strings
+			to all uppercase/lowercase
 
-    */
-    std::string s1 = "Batman", s2 = "Aquaman";
-    int compResult = _stricmp(s1.c_str(), s2.c_str());
-    //OR...
-    int compareResult = toUpper(s1).compare(toUpper(s2));
+	*/
+	std::string s1 = "Batman", s2 = "Aquaman";
+	int compResult = _stricmp(s1.c_str(), s2.c_str());
+	//OR...
+	int compareResult = toUpper(s1).compare(toUpper(s2));
 
 
 
